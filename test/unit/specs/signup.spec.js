4d469ccac26b314bf60884b6717ff79249947051
Mick Carter <bang.bang.software@gmail.com>
2017-03-03 19:34:20 +0000

    Broken testing

--- test/unit/specs/signup.spec.js
+++ test/unit/specs/signup.spec.js
@@ -1,11 +1,13 @@
 import Vue from 'vue'
-import signup from 'src/components/signup/signup'
+//import signup from 'src/components/signup/signup'
+import signup from 'src/components/signup/signup.vue'
 
 describe('signup.vue', () => {
-  it('should render correct contents', () => {
-    const Constructor = Vue.extend(signup)
-    const vm = new Constructor().$mount()
-    expect(vm.$el.querySelector('.signup h1').textContent)
-      .to.equal('Quick Start Up')
-  })
+   it('should render correct contents', () => {
+      const Constructor = Vue.extend(signup)
+      const vm = new Constructor().$mount()
+      console.log(vm.methods)
+      expect(vm.$el.querySelector('.md-title h1').textContent)
+         .to.equal('Quick Start Up')
+   })
 })
