f7d2a317d8ad29cf37cd7dc69b230628b529db5c
Mick Carter <bang.bang.software@gmail.com>
2017-03-15 09:59:34 +0000

    Better testing

--- test/unit/specs/story.spec.js
+++ test/unit/specs/story.spec.js
@@ -34,16 +34,26 @@ describe('story.vue', () => {
       expect(store.state.session.project.stories.length).to.equal(0)
    })
 
-   it('should NOT add an invalid story', () => {
+   it('should NOT add an invalid story with no fields filled in', () => {
+      story.methods.postStory()
+      expect(store.state.session.project.stories.length).to.equal(0)
+   })
+
+   it('should NOT add an invalid story with one field filled in', () => {
+      store.commit('title', 'tester')
+      story.methods.postStory()
+      expect(store.state.session.project.stories.length).to.equal(0)
+   })
+
+   it('should NOT add an invalid story with two field filled in', () => {
+      store.commit('desc', 'tesc desc')
       story.methods.postStory()
       expect(store.state.session.project.stories.length).to.equal(0)
    })
 
    it('should add a valid story', () => {
-      store.commit('title','tester')
-      store.commit('desc','tesc desc')
-      var crit = {}     
-      store.commit('acceptance',crit)
+      var crit = {}
+      store.commit('acceptance', crit)
       story.methods.postStory()
       expect(store.state.session.project.stories.length).to.equal(1)
    })
