b5d86e6e0c3499239f97b18beab690a881581868
Mick Carter <bang.bang.software@gmail.com>
2017-03-28 19:05:58 +0100

    Promise a redirect

--- src/direct.js
+++ src/direct.js
@@ -3,42 +3,53 @@ import store from './store.js'
 import db from './dbase.js'
 
 const service = (me) => {
-   let rtn = 'story'
-   try {
-      console.log('HOW DO I direct the route????')
-      console.log(router)
-      console.log(me)
-      if (!window) {
-         return 'no window'
-      }
-      if (typeof me.currentProject === 'undefined') {
-         window.location.href = '#/start'
-         return 'start'
-      }
-      db.get(me.currentProject).then(project => {
-         store.commit('project', project)
-         if (project.email.owner.email === me.email) {
-            console.log('Owner has logged in')
-            window.location.href = '#/story'
-         } else {
-            console.log('A member ha logged in?')
-               // @TODO go through members and see if they are in the project
-         }
-      }).catch(err => {
-         console.error('Cannot find "' + me.currentProject + '".')
-         console.error(err)
-         window.location.href = '#/start/' + me.currentProject
-         rtn = 'start'
-      })
-
-      if (typeof me.hours === 'undefined' || me.hours.length === 0) {
+   const whereNow = (user, resolve) => {
+      if (typeof user.hours === 'undefined' || user.hours.length === 0) {
          window.location.href = '#/member'
-         rtn = 'member'
-      } else {}
-   } catch (er) {
-      console.log(er)
+         resolve('member')
+      } else {
+         window.location.href = '#/story'
+         resolve('story')
+      }
    }
-   return rtn
+   return new Promise((resolve, reject) => {
+      try {
+         console.log('HOW DO I direct the route????')
+         console.log(router)
+         console.log(me)
+         if (!window) {
+            return 'no window'
+         }
+         if (typeof me.currentProject === 'undefined') {
+            window.location.href = '#/start'
+            resolve('start')
+         }
+         db.get(me.currentProject).then(project => {
+            store.commit('project', project)
+            const owner = project.owner
+            if (owner.email === me.email) {
+               console.log('Owner has logged in')
+               whereNow(owner, resolve)
+            } else {
+               console.log('A member has logged in?')
+               const memberList = project.members.filter(member => member.email === me.email)
+               if (memberList.length === 0) {
+                  reject('Not in project')
+               } else {
+                  whereNow(memberList[0], resolve)
+               }
+            }
+         }).catch(err => {
+            console.error('Cannot find "' + me.currentProject + '".')
+            console.error(err)
+            window.location.href = '#/start/' + me.currentProject
+            resolve('start')
+         })
+      } catch (er) {
+         console.log(er)
+         reject(er)
+      }
+   })
 }
 
 export default service
