c105be9b892062e6ead99d2363247a852ca71ec9
rjgallac@hotmail.com <rjgallac@hotmail.com>
2017-06-05 22:19:18 +0100

    start of delete story

--- src/pages/story/list/storycard/actions/actions.js
+++ src/pages/story/list/storycard/actions/actions.js
@@ -1,15 +1,17 @@
+import store from '../../../storyStore.js'
 import template from './actions.html'
 
 export default {
     name: 'actions',
     template,
-    props: ['story'],
+    props: ['story', 'c'],
     methods: {
-        editStory: function() {
+        editStory: function(story) {
             console.log('edit')
         },
-        removeStory: function(story) {
+        removeStory: function(story, c) {
             console.log('remove')
+            //store.deleteStory(story, c)
         },
         breakStory: function(story) {
             console.log('break')
