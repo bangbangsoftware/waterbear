b7f8df274c74c43eaa45110861596d4ff814c601
Mick Carter <bang.bang.software@gmail.com>
2017-07-04 05:35:41 +0100

    Reading for a spare skills chart

--- src/pages/plan/chart/balanceChart.js
+++ src/pages/plan/chart/balanceChart.js
@@ -8,9 +8,28 @@ import store from 'store'
 
 import skills from './skills.js'
 
+const getBoth = (skills, results) => {
+   const just = {}
+   skills.forEach(skill => {
+      just[skill] = results[skill]
+   })
+
+   const needs = Object.values(just).map(r => r.need)
+   const gots = Object.values(just).map(r => r.got)
+   console.log('gots', gots)
+   console.log('needs', needs)
+   return {
+      needs,
+      gots,
+      skills
+   }
+}
+
 const comp = Bar.extend({
    mounted() {
       const project = store.state.session.project
+         // @TODO store results in store state and use in this graph
+         // and spare skill graph....
       console.log('The project is %o', project)
       const sprint = project.sprints[store.state.session.sprintIndex]
       const members = project.members
@@ -24,33 +43,21 @@ const comp = Bar.extend({
       const results = skills.skillBalance(members, now, fortnightAway, sprint)
       console.log('results', results)
 
-      const chart = (skills, results) => {
-         const just = {}
-         skills.forEach(skill => {
-            just[skill] = results[skill]
-         })
-
-         const needs = Object.values(just).map(r => r.need)
-         const gots = Object.values(just).map(r => r.got)
-         console.log('gots', gots)
-         console.log('needs', needs)
-         this.renderChart({
-            labels: skills,
-            datasets: [{
-               label: 'Need',
-               backgroundColor: '#f87979',
-               data: needs
-            }, {
-               label: 'Got',
-               backgroundColor: '#180079',
-               data: gots
-            }]
-         })
-      }
-
       /* Don't care about all members skills just whats in the sprint */
       const sprintSkills = skills.sprint(sprint)
-      chart(sprintSkills, results)
+      const display = getBoth(sprintSkills, results)
+      this.renderChart({
+         labels: display.skills,
+         datasets: [{
+            label: 'Need',
+            backgroundColor: '#f87979',
+            data: display.needs
+         }, {
+            label: 'Got',
+            backgroundColor: '#180079',
+            data: display.gots
+         }]
+      })
 
       /* const allSkills = Object.keys(results)
       const memberSkills = []
@@ -59,7 +66,7 @@ const comp = Bar.extend({
             memberSkills.push(key)
          }
       })
-      chart(memberSkills, results)
+      const display = getBoth(memberSkills, results)
       */
    }
 })
