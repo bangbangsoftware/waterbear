54811d387c04b4b8d1ef3d6f7f312ac6f74a5f70
Mick Carter <cick.marter@gmail.com>
2017-10-15 18:13:14 +0100

    Better hours...

--- src/pages/team/team.js
+++ src/pages/team/team.js
@@ -13,23 +13,32 @@ const MOY = ['January',
 
 const getPostfix = n => 'th'
 
-const makeUnique = (list, key) => {
-    const keys = []
-    return list.filter(item => {
-        if (keys.indexOf(item[key]) > -1) {
-            return false
-        }
-        keys.push(item[key])
-        return true
-    })
-}
+const NORMAL = 0
+const OFF = 2
+
+const DS = [{
+    id: 0,
+    display: '',
+    off: false
+}, {
+    id: 1,
+    display: 'WFH',
+    off: false
+}, {
+    id: 2,
+    display: 'OFF',
+    off: true
+}, {
+    id: 3,
+    display: 'SICK',
+    off: true
+}]
 
 const comp = {
     name: 'team',
     beforeCreate,
     template,
-    data: () => {
-        const project = store.state.session.project
+    data: function() {
         const days = []
         const now = new Date()
         const dd = now.getDate()
@@ -45,24 +54,26 @@ const comp = {
             lastMonth = moy
             const format = ' ' + DOW[date.getDay()] + ' the ' + dom + getPostfix(dom)
             const colour = (day === 0) ? 'green' : (day < 0) ? 'grey' : 'white'
-            const state = ''
             const data = {
                 format,
                 date,
                 newMonth,
-                colour,
-                state
+                colour
             }
             days.push(data)
         }
+        // Seperate method and should insert days if
+        // they don't exist and put state on session of the team....
+        const project = store.state.session.project
         const list = JSON.parse(JSON.stringify(project.members))
         list.push(project.owner)
-        const members = makeUnique(list, 'name').map(member => {
-            member.days = days.map(day => day.state)
+        const members = comp.makeUnique(list, 'name').map(member => {
+            member.days = days.map(day => comp.dayState(day.date, member.days))
+                // member.days = days.map(day => DS[NORMAL])
             return member
         })
         console.log('we have %o members', members.length)
-        return {
+        const d = {
             session: store.state.session,
             menu: store.state.menu,
             days,
@@ -71,6 +82,8 @@ const comp = {
             x: 0,
             y: 0
         }
+        console.log(d)
+        return d
     },
     methods: {
         show(e) {
@@ -80,12 +93,45 @@ const comp = {
             this.y = e.clientY
         },
         save: () => {},
-        wfh: (c, day) => {
-            console.log('Working from home %o, %o', c, day.format)
-        },
-        sick: (c) => {},
-        holiday: (c) => {}
+        toggle: function(member, day) {
+            console.log('toggling from %o and %o', member, day)
+            const currentState = this.members[member].days[day]
+            const nextState = comp.cycle(currentState)
+            this.members[member].days[day] = nextState
+            console.log(nextState)
+            return nextState
+        }
     }
+}
+
+comp.makeUnique = (list, key) => {
+    const keys = []
+    return list.filter(item => {
+        if (keys.indexOf(item[key]) > -1) {
+            return false
+        }
+        keys.push(item[key])
+        return true
+    })
+}
+
+comp.cycle = state => {
+    const next = (state.id + 1 === DS.length) ? 0 : state.id + 1
+    return DS[next]
+}
+
+comp.dayState = (date, days) => {
+    const wd = date.getDay()
+    const day = days[wd]
+    const nightHours = day.night.map(nt => (nt.on) ? 1 : 0).reduce((total, curr) => total + curr)
+    const dayHours = day.day.map(dy => (dy.on) ? 1 : 0).reduce((total, curr) => total + curr)
+    const total = dayHours + nightHours
+    if (total === 0) {
+        return DS[OFF]
+    }
+    const normal = DS[NORMAL]
+    normal.display = total + ' hours'
+    return normal
 }
 
 Vue.component('team', comp)
--- src/pages/team/team.test.js
+++ src/pages/team/team.test.js
@@ -1,6 +1,95 @@
 import comp from './team.js'
 
-it('should have a bunch of functions', () => {
-   const app = comp.methods
-   expect(typeof app.sick).toBe('function')
+it('should have a toggle method', () => {
+    const app = comp.methods
+    expect(typeof app.toggle).toBe('function')
+})
+
+it('should have some extra functions', () => {
+    expect(typeof comp.makeUnique).toBe('function')
+    expect(typeof comp.cycle).toBe('function')
+    expect(typeof comp.dayState).toBe('function')
+})
+
+
+it('should be able to calculate a day state', () => {
+    const days = [{
+        'day': [{
+            'name': '7am',
+            'on': false
+        }, {
+            'name': '8am',
+            'on': false
+        }, {
+            'name': '9am',
+            'on': false
+        }, {
+            'name': '10am',
+            'on':true 
+        }, {
+            'name': '11am',
+            'on': true 
+        }, {
+            'name': '12am',
+            'on': false
+        }, {
+            'name': '1pm',
+            'on': true
+        }, {
+            'name': '2pm',
+            'on': true 
+        }, {
+            'name': '3pm',
+            'on':true 
+        }, {
+            'name': '4pm',
+            'on': true
+        }, {
+            'name': '5pm',
+            'on': true
+        }, {
+            'name': '6pm',
+            'on': true
+        }, {
+            'name': '7pm',
+            'on': false
+        }],
+        'night': [{
+            'name': '8pm',
+            'on': true 
+        }, {
+            'name': '9pm',
+            'on': false
+        }, {
+            'name': '10pm',
+            'on': false
+        }, {
+            'name': '11pm',
+            'on': false
+        }, {
+            'name': '12pm',
+            'on': false
+        }, {
+            'name': '1am',
+            'on': false
+        }, {
+            'name': '2am',
+            'on': false
+        }, {
+            'name': '3am',
+            'on': false
+        }, {
+            'name': '4am',
+            'on': false
+        }, {
+            'name': '5am',
+            'on': false
+        }, {
+            'name': '6am',
+            'on': false
+        }]
+    }]
+    const date = new Date(2017, 9, 15, 10, 10, 10)
+    const result = comp.dayState(date, days)
+    expect(result.display).toBe('9 hours')
 })
