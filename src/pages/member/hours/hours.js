8f94ad5aa2b6f29b7a75547ee3eda1e26578865e
Mick Carter <cick.marter@gmail.com>
2017-11-21 14:47:22 +0000

    Toggling time, wfh and on

--- src/pages/member/hours/hours.js
+++ src/pages/member/hours/hours.js
@@ -5,39 +5,39 @@ import './hours.css'
 import defaults from './default.js'
 
 const comp = {
-   name: 'hours',
-   template,
-   data: function() {
-      return {
-         session: store.state.session,
-         days: store.state.session.user.days
-      }
-   },
-   beforeCreate: () => {
-      const storedDays = store.state.session.user.days
-      if (typeof storedDays !== 'undefined') {
-         console.log('Already have default working hours')
-         return
-      }
-      const days = defaults()
-      days.forEach(day => {
-         store.commit('day', day)
-      })
-   },
-   methods: {
-      toggleDay: (day, hour) => {
-         store.commit('toggleDay', {
-            day,
-            hour
-         })
-      },
-      toggleNight: (day, hour) => {
-         store.commit('toggleNight', {
-            day,
-            hour
-         })
-      }
-   }
+    name: 'hours',
+    template,
+    data: function() {
+        return {
+            session: store.state.session,
+            days: store.state.session.user.days
+        }
+    },
+    beforeCreate: () => {
+        const storedDays = store.state.session.user.days
+        if (typeof storedDays !== 'undefined') {
+            console.log('Already have default working hours')
+            return
+        }
+        const days = defaults()
+        days.forEach(day => {
+            store.commit('day', day)
+        })
+    },
+    methods: {
+        toggleDay: (day, hour) => {
+            store.commit('toggleDay', {
+                day,
+                hour
+            })
+        },
+        toggleNight: (day, hour) => {
+            store.commit('toggleNight', {
+                day,
+                hour
+            })
+        }
+    }
 }
 Vue.component('hours', comp)
 export default comp
--- src/pages/member/memberStore.js
+++ src/pages/member/memberStore.js
@@ -1,14 +1,45 @@
+const nextState = hour => {
+    if (hour.state !== undefined && hour.state === 'on') {
+        return 'off'
+    }
+    if (hour.state === undefined || hour.state === 'wfh') {
+        return 'on'
+    }
+    return 'wfh'
+}
+
+const currentName = (state, time, p) => {
+    const direction = time.hour > 0 ? -1 : 1
+    const next = time.hour + direction
+    const hour = state.session.user.days[time.day][p][next]
+    const number = Number.parseInt(hour.name) + (direction * -1)
+    if (number > 12) {
+        const postfix = (hour.name.indexOf('am') > -1) ? 'pm' : 'am'
+        return '1' + postfix
+    }
+    const postfix = (hour.name.indexOf('pm') > -1) ? 'pm' : 'am'
+    return number + postfix
+}
+
+// @TODO the hour is the index not the value needs remapping where restoring the label
 export default {
     nick: (state, name) => {
         state.session.user.nick = name
     },
     toggleNight: (state, time) => {
-        const now = state.session.user.days[time.day].night[time.hour].on
-        state.session.user.days[time.day].night[time.hour].on = !now
+        const hour = state.session.user.days[time.day].night[time.hour]
+        const name = currentName(state, time, 'night')
+        hour.state = nextState(hour)
+        hour.name = (hour.state === 'off') ? name : hour.state
+        state.session.user.days[time.day].night[time.hour] = hour
+        state.session.change = time
+        return hour
     },
     toggleDay: (state, time) => {
         const hour = state.session.user.days[time.day].day[time.hour]
-        hour.on = !hour.on
+        const name = currentName(state, time, 'day')
+        hour.state = nextState(hour)
+        hour.name = (hour.state === 'off') ? name : hour.state
         state.session.user.days[time.day].day[time.hour] = hour
         state.session.change = time
         return hour
